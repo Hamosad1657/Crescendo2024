c04ef8f26c6a666e515d6708f3348469e4846a10
Hamosad 1657 <hamosadr@gmail.com>
2024-01-14 15:42:43 +0200

    Changed idleMode to neutralMode

--- src/main/kotlin/frc/robot/subsystems/intake/IntakeConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/IntakeConstants.kt
@@ -3,7 +3,7 @@ package frc.robot.subsystems.intake
 import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration
 
 object IntakeConstants {
-	const val output = 0.0 // TODO: Do tests and decide optimal intake output
-	val supplyCurrentLimitConfiguration =
-		SupplyCurrentLimitConfiguration(true, 40.0, 60.0, 0.01) // TODO: decide optimal intake current limit configs
+	const val OUTPUT = 0.0 // TODO: Do tests and decide optimal intake output
+	val SUPPLY_CURRENT_LIMIT =
+		SupplyCurrentLimitConfiguration(true, 30.0, 35.0, 0.05) // TODO: decide optimal intake current limit configs
 }
--- src/main/kotlin/frc/robot/subsystems/intake/IntakeSubsystem.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/IntakeSubsystem.kt
@@ -2,7 +2,6 @@ package frc.robot.subsystems.intake
 
 import com.ctre.phoenix.motorcontrol.NeutralMode
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX
-import com.revrobotics.CANSparkBase.IdleMode
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import frc.robot.RobotMap
 import frc.robot.subsystems.intake.IntakeConstants as Constants
@@ -10,21 +9,17 @@ import frc.robot.subsystems.intake.IntakeConstants as Constants
 object IntakeSubsystem : SubsystemBase() {
 	private val motor = WPI_TalonSRX(RobotMap.Intake.MOTOR_ID)
 
-	var idleMode: IdleMode = IdleMode.kCoast
+	// TODO: Change default neutral mode
+	var neutralMode: NeutralMode = NeutralMode.Coast
 		set(value) {
-			when (value) {
-				IdleMode.kBrake -> motor.setNeutralMode(NeutralMode.Brake)
-				IdleMode.kCoast -> motor.setNeutralMode(NeutralMode.Coast)
-			}
+			motor.setNeutralMode(value)
 			field = value
 		}
 
 	init {
 		motor.inverted = false // TODO: Verify positive output intakes
-		motor.setNeutralMode(NeutralMode.Coast) // TODO: Decide intake idle mode
-		motor.configSupplyCurrentLimit(Constants.supplyCurrentLimitConfiguration)
+		motor.configSupplyCurrentLimit(Constants.SUPPLY_CURRENT_LIMIT)
 		motor.isSafetyEnabled = true
-		idleMode = IdleMode.kCoast
 	}
 
 	fun set(percentOutput: Double) {
