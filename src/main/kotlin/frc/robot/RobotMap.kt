cf2952bae4b71c5ca94c6c51b401f107344b0f3f
Hamosad 1657 <hamosadr@gmail.com>
2024-01-14 19:10:38 +0200

    Mark: added motors

--- src/main/kotlin/frc/robot/RobotMap.kt
+++ src/main/kotlin/frc/robot/RobotMap.kt
@@ -4,30 +4,33 @@ package frc.robot
  * This objects contains DIO channel numbers, CAN IDs, USB port numbers and the like.
  */
 object RobotMap {
-	const val DRIVER_A_CONTROLLER_PORT = 0
-	const val DRIVER_B_CONTROLLER_PORT = 1
+    const val DRIVER_A_CONTROLLER_PORT = 0
+    const val DRIVER_B_CONTROLLER_PORT = 1
 
-	object Arm {
+    object Arm {
 
-	}
+    }
 
-	object Climber {
-		
-	}
+    object Climber {
+        const val FIRST_RIGHT_MOTOR_ID = 0
+        const val SECOND_RIGHT_MOTOR_ID = 0
+        const val FIRST_LEFT_MOTOR_ID = 0
+        const val SECOND_LEFT_MOTOR_ID = 0
+    }
 
-	object Grabber {
+    object Grabber {
 
-	}
+    }
 
-	object Intake {
+    object Intake {
 
-	}
+    }
 
-	object Shooter {
+    object Shooter {
 
-	}
+    }
 
-	object Swerve {
+    object Swerve {
 
-	}
+    }
 }
--- src/main/kotlin/frc/robot/subsystems/climbing/ClimbingSubsystem.kt
+++ src/main/kotlin/frc/robot/subsystems/climbing/ClimbingSubsystem.kt
@@ -1,6 +1,17 @@
 package frc.robot.subsystems.climbing
 
+import com.hamosad1657.lib.motors.HaTalonFX
 import edu.wpi.first.wpilibj2.command.SubsystemBase
+import frc.robot.RobotMap.Climber as RobotMap
 
 object ClimbingSubsystem : SubsystemBase() {
+    private val firstRightMotor = HaTalonFX(RobotMap.FIRST_RIGHT_MOTOR_ID)
+    private val secondRightMotor = HaTalonFX(RobotMap.SECOND_RIGHT_MOTOR_ID).apply {
+        set
+    }
+
+    private val firstLeftMotor = HaTalonFX(RobotMap.FIRST_LEFT_MOTOR_ID)
+    private val secondLeftMotor = HaTalonFX(RobotMap.SECOND_LEFT_MOTOR_ID)
+
+
 }
