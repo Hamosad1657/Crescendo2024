2cf21bed9ff89058c73cd7c9cf9a50364791c1b0
Hamosad-1657 <145373383+Hamosad-1657@users.noreply.github.com>
2024-01-15 19:54:34 +0200

    Mark
    Added setRotationSetpoint func for both speed limited motion and unlimited motion, added motion magic config

--- src/main/kotlin/frc/robot/subsystems/climbing/ClimbingConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/climbing/ClimbingConstants.kt
@@ -1,8 +1,28 @@
 package frc.robot.subsystems.climbing
 
+import com.ctre.phoenix6.configs.HardwareLimitSwitchConfigs
+import com.ctre.phoenix6.configs.MotionMagicConfigs
+import com.ctre.phoenix6.signals.ForwardLimitSourceValue
+import com.ctre.phoenix6.signals.ForwardLimitTypeValue
 import com.hamosad1657.lib.math.PIDGains
 
 object ClimbingConstants {
     // TODO: Tune PID
     val PID_GAINS = PIDGains(0.0, 0.0, 0.0)
+
+    const val FULLY_CLOSED_ROTATIONS_COUNT = 0.0;
+    const val FULLY_OPEN_ROTATIONS_COUNT = 0.0
+
+    val FALCON_HARDWARE_LIMITS_CONFIG = HardwareLimitSwitchConfigs().apply {
+        ForwardLimitEnable = true
+        ForwardLimitAutosetPositionEnable = true
+        ForwardLimitAutosetPositionValue = FULLY_CLOSED_ROTATIONS_COUNT
+        ForwardLimitType = ForwardLimitTypeValue.NormallyOpen
+        ForwardLimitSource = ForwardLimitSourceValue.LimitSwitchPin
+    }
+
+    const val MAX_VELOCITY = 0.0;
+    val MOTION_MAGIC_CONFIG = MotionMagicConfigs().apply {
+        MotionMagicCruiseVelocity = MAX_VELOCITY
+    }
 }
--- src/main/kotlin/frc/robot/subsystems/climbing/ClimbingSubsystem.kt
+++ src/main/kotlin/frc/robot/subsystems/climbing/ClimbingSubsystem.kt
@@ -1,27 +1,54 @@
 package frc.robot.subsystems.climbing
 
 import com.ctre.phoenix6.controls.Follower
+import com.ctre.phoenix6.controls.MotionMagicVoltage
+import com.ctre.phoenix6.controls.PositionVoltage
+import com.ctre.phoenix6.signals.NeutralModeValue
 import com.hamosad1657.lib.motors.HaTalonFX
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import frc.robot.RobotMap.Climber as ClimberMap
 import frc.robot.subsystems.climbing.ClimbingConstants as Constants
 
 object ClimbingSubsystem : SubsystemBase() {
-    private val rightMotor1 = HaTalonFX(ClimberMap.RIGHT_MOTOR_1_ID).apply {
-        configPIDGains(Constants.PID_GAINS)
-    }
-    private val rightMotor2 = HaTalonFX(ClimberMap.RIGHT_MOTOR_2_ID).apply {
-        // TODO: Check if follower motor should oppose master motor or not
-        setControl(Follower(ClimberMap.RIGHT_MOTOR_1_ID, false))
-    }
-
     private val leftMotor1 = HaTalonFX(ClimberMap.LEFT_MOTOR_1_ID).apply {
         configPIDGains(Constants.PID_GAINS)
+        configurator.apply(Constants.FALCON_HARDWARE_LIMITS_CONFIG)
+        configurator.apply(Constants.MOTION_MAGIC_CONFIG)
+
     }
     private val leftMotor2 = HaTalonFX(ClimberMap.LEFT_MOTOR_2_ID).apply {
         // TODO: Check if follower motor should oppose master motor or not
         setControl(Follower(ClimberMap.LEFT_MOTOR_1_ID, false))
     }
 
-    
+    private val rightMotor1 = HaTalonFX(ClimberMap.RIGHT_MOTOR_1_ID).apply {
+        configPIDGains(Constants.PID_GAINS)
+        configurator.apply(Constants.FALCON_HARDWARE_LIMITS_CONFIG)
+        configurator.apply(Constants.MOTION_MAGIC_CONFIG)
+    }
+    private val rightMotor2 = HaTalonFX(ClimberMap.RIGHT_MOTOR_2_ID).apply {
+        // TODO: Check if follower motor should oppose master motor or not
+        setControl(Follower(ClimberMap.RIGHT_MOTOR_1_ID, false))
+    }
+
+
+    var neutralMode = NeutralModeValue.Brake
+        set(value) {
+            leftMotor1.setNeutralMode(value)
+            leftMotor2.setNeutralMode(value)
+            rightMotor1.setNeutralMode(value)
+            rightMotor2.setNeutralMode(value)
+            field = value
+        }
+
+    fun setRotationSetpoint(rotations: Double, limitSpeed: Boolean) {
+        if (limitSpeed) {
+            leftMotor1.setControl(MotionMagicVoltage(rotations))
+            rightMotor1.setControl(MotionMagicVoltage(rotations))
+        } else {
+            leftMotor1.setControl(PositionVoltage(rotations))
+            rightMotor1.setControl(PositionVoltage(rotations))
+        }
+    }
+
 }
