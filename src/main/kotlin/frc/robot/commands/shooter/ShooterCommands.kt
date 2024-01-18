b107f0a9a76e47c891476216ec4fc28f87c48716
Hamosad-1657 <145373383+Hamosad-1657@users.noreply.github.com>
2024-01-18 14:44:45 +0200

    Shaked: Started skeleton of shooter commands

--- src/main/kotlin/frc/robot/commands/shooter/ShooterCommands.kt
+++ src/main/kotlin/frc/robot/commands/shooter/ShooterCommands.kt
@@ -1,7 +1,32 @@
 package frc.robot.commands.shooter
 
-import edu.wpi.first.math.geometry.Pose3d
+import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand
+import frc.robot.subsystems.shooter.ShooterConstants
+import frc.robot.subsystems.shooter.ShooterSubsystem
 
-private fun calculateShooterState(robotPosition: Pose3d, targetPosition: Pose3d) {
-    // TODO: Continue
+/**
+ * To be used in a command group.
+ */
+fun ShooterSubsystem.loadCommand(): Command {
+    
 }
+
+/**
+ * To be used in a command group.
+ */
+fun ShooterSubsystem.waitUntilShooterInTolerance() = WaitUntilCommand {}
+
+/**
+ * No end condition. To be used in a command group.
+ */
+fun ShooterSubsystem.getToShooterStateCommand(): Command {}
+
+/**
+ * Time between when loading started to when the note is shot.
+ * It might be a little different in different speeds, so put here
+ * it's maximum value.
+ */
+private const val SHOOT_TIME = 0.0 // TODO: Measure SHOOT_TIME
+
+fun ShooterSubsystem.shootCommand(): Command {}
