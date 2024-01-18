47b6b4cefe73de0bf40d0a8b52769a0110dff4a5
Hamosad-1657 <145373383+Hamosad-1657@users.noreply.github.com>
2024-01-18 09:17:05 +0200

    Shaked: I started calculateShooterState and then had to leave

---
+++ src/main/kotlin/frc/robot/commands/shooter/ShooterCommands.kt
@@ -0,0 +1,7 @@
+package frc.robot.commands.shooter
+
+import edu.wpi.first.math.geometry.Pose3d
+
+private fun calculateShooterState(robotPosition: Pose3d, targetPosition: Pose3d) {
+    // TODO: Continue
+}
