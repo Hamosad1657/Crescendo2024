1dbf9ad7d54f3caeb549b7730f2f66e9f3a0b02e
Hamosad-1657 <145373383+Hamosad-1657@users.noreply.github.com>
2024-01-23 09:02:03 +0200

    Shaked: Organized arm subsystem

--- src/main/kotlin/frc/robot/subsystems/arm/ArmConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/arm/ArmConstants.kt
@@ -3,5 +3,5 @@ package frc.robot.subsystems.arm
 object ArmConstants {
     const val DEFAULT_OUTPUT = 0.0
     const val STATOR_CURRENT_AT_REVERSE_LIMIT = 0.0
-    const val AT_REVERSE_LIMIT_TIME = 0.0
+    const val AT_REVERSE_LIMIT_TIME_SEC = 0.0
 }
--- src/main/kotlin/frc/robot/subsystems/arm/ArmSubsystem.kt
+++ src/main/kotlin/frc/robot/subsystems/arm/ArmSubsystem.kt
@@ -5,11 +5,11 @@ import com.hamosad1657.lib.motors.HaTalonSRX
 import edu.wpi.first.wpilibj.DigitalInput
 import edu.wpi.first.wpilibj.Timer
 import edu.wpi.first.wpilibj2.command.SubsystemBase
+import frc.robot.subsystems.arm.ArmConstants.AT_REVERSE_LIMIT_TIME_SEC
+import frc.robot.subsystems.arm.ArmConstants.STATOR_CURRENT_AT_REVERSE_LIMIT
 import frc.robot.RobotMap.Arm as ArmMap
 
 object ArmSubsystem : SubsystemBase() {
-    // TODO: Check if limit is wired normally false or normally false.
-
     private val leftMotor = HaTalonSRX(ArmMap.MOTOR_ID)
     private val rightMotor = HaTalonSRX(ArmMap.MOTOR_ID)
 
@@ -25,21 +25,28 @@ object ArmSubsystem : SubsystemBase() {
         start()
     }
 
-    fun setNeutralMode(neutralMode: NeutralMode) {
-        leftMotor.setNeutralMode(neutralMode)
-        rightMotor.setNeutralMode(neutralMode)
-    }
+    var neutralMode: NeutralMode = NeutralMode.Brake
+        set(value) {
+            leftMotor.setNeutralMode(value)
+            rightMotor.setNeutralMode(value)
+            field = value
+        }
 
     fun set(percentOutput: Double) {
         leftMotor.set(percentOutput)
     }
 
+    // TODO: Check if limit switch is wired normally false or normally true.
     val isAtForwardLimit get() = forwardLimit.get()
 
+    /**
+     * True if stator current was over [STATOR_CURRENT_AT_REVERSE_LIMIT]
+     * for over [AT_REVERSE_LIMIT_TIME_SEC].
+     */
     val isAtReverseLimit: Boolean
         get() {
-            if ((leftMotor.statorCurrent >= ArmConstants.STATOR_CURRENT_AT_REVERSE_LIMIT) &&
-                reverseLimitTimer.hasElapsed(ArmConstants.AT_REVERSE_LIMIT_TIME)
+            if ((leftMotor.statorCurrent >= STATOR_CURRENT_AT_REVERSE_LIMIT) &&
+                reverseLimitTimer.hasElapsed(AT_REVERSE_LIMIT_TIME_SEC)
             ) {
                 return true
             } else {
