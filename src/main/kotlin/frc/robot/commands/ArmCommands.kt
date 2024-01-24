8af56024626691825f454c8e14384b49f18b980d
Hamosad-1657 <145373383+Hamosad-1657@users.noreply.github.com>
2024-01-24 09:00:54 +0200

    Shaked: Refactored arm commands

--- src/main/kotlin/frc/robot/commands/ArmCommands.kt
+++ src/main/kotlin/frc/robot/commands/ArmCommands.kt
@@ -1,22 +1,35 @@
 package frc.robot.commands
 
+import com.hamosad1657.lib.commands.andThen
 import com.hamosad1657.lib.commands.finallyDo
 import com.hamosad1657.lib.commands.until
 import com.hamosad1657.lib.commands.withName
 import edu.wpi.first.wpilibj2.command.Command
-import frc.robot.subsystems.arm.ArmConstants.DEFAULT_OUTPUT
+import frc.robot.subsystems.arm.ArmConstants.FOLD_OUTPUT
+import frc.robot.subsystems.arm.ArmConstants.OPEN_OUTPUT
+import frc.robot.subsystems.arm.ArmConstants.STAY_FOLDED_OUTPUT
 import frc.robot.subsystems.arm.ArmSubsystem
 
 /** - Requirements: arm. */
 fun ArmSubsystem.openCommand(): Command {
     return withName("open") {
-        run { set(DEFAULT_OUTPUT) } until ::isAtForwardLimit finallyDo { set(0.0) }
+        run { set(OPEN_OUTPUT) } until ::isAtForwardLimit finallyDo { set(0.0) }
     }
 }
 
 /** - Requirements: arm. */
 fun ArmSubsystem.foldCommand(): Command {
     return withName("fold") {
-        run { set(-DEFAULT_OUTPUT) } until ::isAtReverseLimit finallyDo { set(0.0) }
+        run { set(FOLD_OUTPUT) } until ::isAtReverseLimit finallyDo { set(0.0) }
     }
+}
+
+fun ArmSubsystem.stayFoldedCommand(): Command {
+    return withName("stay folded") {
+        run { set(STAY_FOLDED_OUTPUT) } finallyDo { set(0.0) }
+    }
+}
+
+fun ArmSubsystem.foldAndStayFoldedCommand(): Command {
+    return withName("fold & stay folded") { foldCommand() andThen stayFoldedCommand() }
 }
--- src/main/kotlin/frc/robot/subsystems/arm/ArmConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/arm/ArmConstants.kt
@@ -1,7 +1,7 @@
 package frc.robot.subsystems.arm
 
 object ArmConstants {
-    const val DEFAULT_OUTPUT = 0.0
-    const val STATOR_CURRENT_AT_REVERSE_LIMIT = 0.0
-    const val AT_REVERSE_LIMIT_TIME_SEC = 0.0
+    const val OPEN_OUTPUT = 0.0
+    const val FOLD_OUTPUT = 0.0
+    const val STAY_FOLDED_OUTPUT = 0.0
 }
