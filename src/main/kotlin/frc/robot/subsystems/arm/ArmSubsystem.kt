ee7585221e2c29ec5b62280327e698b5910b46b5
Hamosad-1657 <145373383+Hamosad-1657@users.noreply.github.com>
2024-01-24 08:53:37 +0200

    Shaked: Added reverse limit switch to arm subsystem

--- src/main/kotlin/frc/robot/subsystems/arm/ArmSubsystem.kt
+++ src/main/kotlin/frc/robot/subsystems/arm/ArmSubsystem.kt
@@ -4,10 +4,7 @@ import com.ctre.phoenix.motorcontrol.NeutralMode
 import com.hamosad1657.lib.motors.HaTalonSRX
 import com.hamosad1657.lib.subsystemutils.setNameToClassName
 import edu.wpi.first.wpilibj.DigitalInput
-import edu.wpi.first.wpilibj.Timer
 import edu.wpi.first.wpilibj2.command.SubsystemBase
-import frc.robot.subsystems.arm.ArmConstants.AT_REVERSE_LIMIT_TIME_SEC
-import frc.robot.subsystems.arm.ArmConstants.STATOR_CURRENT_AT_REVERSE_LIMIT
 import frc.robot.RobotMap.Arm as ArmMap
 
 object ArmSubsystem : SubsystemBase() {
@@ -25,10 +22,8 @@ object ArmSubsystem : SubsystemBase() {
     }
 
     private val forwardLimit = DigitalInput(ArmMap.FORWARD_LIMIT_CHANNEL)
+    private val reverseLimit = DigitalInput(ArmMap.REVERSE_LIMIT_CHANNEL)
 
-    private val reverseLimitTimer = Timer().apply {
-        start()
-    }
 
     var neutralMode: NeutralMode = NeutralMode.Brake
         set(value) {
@@ -43,20 +38,5 @@ object ArmSubsystem : SubsystemBase() {
 
     // TODO: Check if limit switch is wired normally false or normally true.
     val isAtForwardLimit get() = forwardLimit.get()
-
-    /**
-     * True if stator current was over [STATOR_CURRENT_AT_REVERSE_LIMIT]
-     * for over [AT_REVERSE_LIMIT_TIME_SEC].
-     */
-    val isAtReverseLimit: Boolean
-        get() {
-            if ((leftMotor.statorCurrent >= STATOR_CURRENT_AT_REVERSE_LIMIT) &&
-                reverseLimitTimer.hasElapsed(AT_REVERSE_LIMIT_TIME_SEC)
-            ) {
-                return true
-            } else {
-                reverseLimitTimer.restart()
-                return false
-            }
-        }
+    val isAtReverseLimit get() = reverseLimit.get()
 }
