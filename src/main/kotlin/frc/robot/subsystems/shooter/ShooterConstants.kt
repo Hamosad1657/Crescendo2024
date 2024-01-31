d352fcdffee2a21f7f8bc6a9fb9bc7eb78009d37
ShakedShiboleth <96763878+ShakedShiboleth@users.noreply.github.com>
2024-01-31 21:56:21 +0200

    Shaked: AddedRotorToSensorRatio for shooter angle and implemented initSendable

--- src/main/kotlin/frc/robot/subsystems/shooter/ShooterConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/shooter/ShooterConstants.kt
@@ -10,54 +10,57 @@ import edu.wpi.first.math.geometry.Rotation2d
 import frc.robot.subsystems.climbing.ClimbingConstants
 
 object ShooterConstants {
-    // TODO: Find velocity and angle tolerances for shooter
-    val VELOCITY_TOLERANCE: AngularVelocity = 0.0.rpm
-    val ANGLE_TOLERANCE = 0.25.degrees
+	// TODO: Find velocity and angle tolerances for shooter
+	val VELOCITY_TOLERANCE: AngularVelocity = 0.0.rpm
+	val ANGLE_TOLERANCE = 0.25.degrees
 
-    val ANGLE_FOR_INTAKE = Rotation2d() // TODO: Find SHOOTER_ANGLE_FOR_INTAKE
+	val ANGLE_FOR_INTAKE = Rotation2d() // TODO: Find SHOOTER_ANGLE_FOR_INTAKE
 
-    /**
-     * Time between when loading started to when the note is shot.
-     * It might be a little different in different speeds, so put here
-     * it's maximum value.
-     */
-    const val SHOOT_TIME_SEC = 0.0 // TODO: Measure SHOOT_TIME_SEC
+	// TODO: Find rotor to CANCoder gear ratio
+	const val ANGLE_MOTOR_TO_CANCODER_GEAR_RATIO = 0.0
 
-    /** 0 should be the starting position (the lowest possible angle). */
-    const val CANCODER_OFFSET_DEG = 0.0
+	/**
+	 * Time between when loading started to when the note is shot.
+	 * It might be a little different in different speeds, so put here
+	 * it's maximum value.
+	 */
+	const val SHOOT_TIME_SEC = 0.0 // TODO: Measure SHOOT_TIME_SEC
 
-    // ShooterState is a data class and not an enum, because we might want to make
-    // a continuous function (robot pose3d to target pose3d) if we have the time.
-    // In the meantime, we will shoot from a few constant positions. Keep instances
-    // of ShooterState as constants.
-    data class ShooterState(val angle: Rotation2d, val velocity: AngularVelocity) {
+	/** 0 should be the starting position (the lowest possible angle). */
+	const val CANCODER_OFFSET_DEG = 0.0
 
-        companion object {
-            // TODO: Test and find the shooter states
-            val TO_AMP = ShooterState(Rotation2d(), 0.0.rpm)
-            val TO_TRAP = ShooterState(Rotation2d(), 0.0.rpm)
+	// ShooterState is a data class and not an enum, because we might want to make
+	// a continuous function (robot pose3d to target pose3d) if we have the time.
+	// In the meantime, we will shoot from a few constant positions. Keep instances
+	// of ShooterState as constants.
+	data class ShooterState(val angle: Rotation2d, val velocity: AngularVelocity) {
 
-            // TODO: Name these shooter states better
-            val TO_SPEAKER_1 = ShooterState(Rotation2d(), 0.0.rpm)
-            val TO_SPEAKER_2 = ShooterState(Rotation2d(), 0.0.rpm)
-            val TO_SPEAKER_3 = ShooterState(Rotation2d(), 0.0.rpm)
+		companion object {
+			// TODO: Test and find the shooter states
+			val TO_AMP = ShooterState(Rotation2d(), 0.0.rpm)
+			val TO_TRAP = ShooterState(Rotation2d(), 0.0.rpm)
 
-            val COLLECT = ShooterState(Rotation2d(), 0.0.rpm)
-        }
-    }
+			// TODO: Name these shooter states better
+			val TO_SPEAKER_1 = ShooterState(Rotation2d(), 0.0.rpm)
+			val TO_SPEAKER_2 = ShooterState(Rotation2d(), 0.0.rpm)
+			val TO_SPEAKER_3 = ShooterState(Rotation2d(), 0.0.rpm)
 
-    val FALCON_HARDWARE_LIMITS_CONFIG = HardwareLimitSwitchConfigs().apply {
-        ForwardLimitEnable = true
-        ForwardLimitAutosetPositionEnable = true
-        ForwardLimitAutosetPositionValue = ClimbingConstants.ClimbingState.REACHING_CHAIN.setpoint
-        ForwardLimitType = ForwardLimitTypeValue.NormallyOpen
-        ForwardLimitSource = ForwardLimitSourceValue.LimitSwitchPin
+			val COLLECT = ShooterState(Rotation2d(), 0.0.rpm)
+		}
+	}
 
-        ReverseLimitEnable = true
-        ReverseLimitAutosetPositionEnable = true
-        ReverseLimitAutosetPositionValue = 0.0
-        ForwardLimitType = ForwardLimitTypeValue.NormallyOpen
-        ForwardLimitSource = ForwardLimitSourceValue.LimitSwitchPin
-    }
+	val FALCON_HARDWARE_LIMITS_CONFIG = HardwareLimitSwitchConfigs().apply {
+		ForwardLimitEnable = true
+		ForwardLimitAutosetPositionEnable = true
+		ForwardLimitAutosetPositionValue = ClimbingConstants.ClimbingState.REACHING_CHAIN.setpoint
+		ForwardLimitType = ForwardLimitTypeValue.NormallyOpen
+		ForwardLimitSource = ForwardLimitSourceValue.LimitSwitchPin
+
+		ReverseLimitEnable = true
+		ReverseLimitAutosetPositionEnable = true
+		ReverseLimitAutosetPositionValue = 0.0
+		ForwardLimitType = ForwardLimitTypeValue.NormallyOpen
+		ForwardLimitSource = ForwardLimitSourceValue.LimitSwitchPin
+	}
 
 }
