d9bf67e7b593299db1e0e9954ade3a3b95394512
ShakedShiboleth <96763878+ShakedShiboleth@users.noreply.github.com>
2024-02-01 12:36:13 +0200

    Made skeleton of calculateShooterState

--- src/main/kotlin/frc/robot/subsystems/shooter/dynamicshooting/DynamicShootingCalculator.kt
+++ src/main/kotlin/frc/robot/subsystems/shooter/dynamicshooting/DynamicShootingCalculator.kt
@@ -7,23 +7,38 @@ import edu.wpi.first.math.geometry.Translation2d
 import edu.wpi.first.math.geometry.Translation3d
 import edu.wpi.first.wpilibj.DriverStation.Alliance
 import frc.robot.subsystems.shooter.ShooterConstants.ShooterState
+import frc.robot.subsystems.shooter.dynamicshooting.DynamicShootingConstants.SHOOTER_PITCH_OFFSET
 import frc.robot.subsystems.shooter.dynamicshooting.DynamicShootingConstants.SHOOTER_PIVOT_TO_CHASSIS_CENTER_TRANSLATION3D
+import frc.robot.subsystems.shooter.dynamicshooting.DynamicShootingConstants.SPEAKER_HEIGHT
 import frc.robot.subsystems.shooter.dynamicshooting.DynamicShootingConstants.SPEAKER_POSITION_METERS_BLUE
 import frc.robot.subsystems.shooter.dynamicshooting.DynamicShootingConstants.SPEAKER_POSITION_METERS_RED
 import kotlin.math.atan2
 
 /** This function assumes the robot is directly facing the speaker. */
-fun calculateShooterState(chassisPositionMeters: Translation2d, alliance: Alliance): ShooterState {
+fun calculateShooterState(chassisPosition2dMeters: Translation2d, alliance: Alliance): ShooterState {
 	val speakerPosition3dMeters = determineSpeakerPosition(alliance)
-	val shooterPosition3dMeters = calculateShooterPosition3dMeters(chassisPositionMeters)
+	val shooterPosition3dMeters = calculateShooterPosition3dMeters(chassisPosition2dMeters)
 
-	val shooterToSpeakerPitch =
-		
+	val shooterToSpeakerFlatDistanceMeters =
+		shooterPosition3dMeters.toTranslation2d()
+			.getDistance(
+				shooterPosition3dMeters.toTranslation2d()
+			)
+
+	val shooterToSpeakerHeightDifferenceMeters = SPEAKER_HEIGHT.meters - shooterPosition3dMeters.z
+
+	val shooterToSpeakerPitch = Rotation2d.fromRadians(
+		atan2(shooterToSpeakerFlatDistanceMeters, shooterToSpeakerHeightDifferenceMeters)
+	)
+
+	val angleSetpoint = shooterToSpeakerPitch + SHOOTER_PITCH_OFFSET
+	val velocitySetpoint = calculateVelocitySetpoint(shooterToSpeakerPitch, shooterToSpeakerFlatDistanceMeters)
+	return ShooterState(angleSetpoint, velocitySetpoint)
 }
 
 private fun calculateVelocitySetpoint(
 	shooterToSpeakerPitch: Rotation2d,
-	shooterToSpeakerFlatDistance: Length
+	shooterToSpeakerFlatDistanceMeters: Double
 ): AngularVelocity {
 	// TODO: Implement
 	return AngularVelocity.fromRpm(0.0)
@@ -36,9 +51,9 @@ private fun determineSpeakerPosition(alliance: Alliance): Translation3d =
 		SPEAKER_POSITION_METERS_RED
 	}
 
-private fun calculateShooterPosition3dMeters(chassisPositionMeters: Translation2d): Translation3d {
-	val robotPosition3dMeters = Translation3d(chassisPositionMeters.x, chassisPositionMeters.y, 0.0)
-	return robotPosition3dMeters + SHOOTER_PIVOT_TO_CHASSIS_CENTER_TRANSLATION3D
+private fun calculateShooterPosition3dMeters(chassisPosition2dMeters: Translation2d): Translation3d {
+	val chassisPosition3dMeters = Translation3d(chassisPosition2dMeters.x, chassisPosition2dMeters.y, 0.0)
+	return chassisPosition3dMeters + SHOOTER_PIVOT_TO_CHASSIS_CENTER_TRANSLATION3D
 }
 
 private fun calculateShooterToSpeakerTranslation3d(
