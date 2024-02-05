168a090f1d6bdb637d96d6a0e1a32bfc7ed9c279
Hamosad-1657 <145373383+Hamosad-1657@users.noreply.github.com>
2024-02-05 13:14:59 +0200

    added camera offset

--- src/main/kotlin/frc/robot/subsystems/vision/Vision.kt
+++ src/main/kotlin/frc/robot/subsystems/vision/Vision.kt
@@ -1,34 +1,26 @@
 package frc.robot.subsystems.vision
 
-import edu.wpi.first.apriltag.AprilTag
+import com.hamosad1657.lib.units.degrees
 import edu.wpi.first.apriltag.AprilTagFieldLayout
-import edu.wpi.first.math.geometry.Pose3d
+import edu.wpi.first.apriltag.AprilTagFields
 import edu.wpi.first.math.geometry.Rotation3d
 import edu.wpi.first.math.geometry.Transform3d
 import edu.wpi.first.math.geometry.Translation3d
-import edu.wpi.first.wpilibj2.command.SubsystemBase
 import org.photonvision.EstimatedRobotPose
 import org.photonvision.PhotonCamera
 import org.photonvision.PhotonPoseEstimator
 import org.photonvision.PhotonPoseEstimator.PoseStrategy
-import kotlin.math.PI
 
-object Vision : SubsystemBase() {
-	private val robotToCamera = Transform3d(Translation3d(), Rotation3d(0.0, 0.0, PI / 2))
+object Vision {
+	private val robotToCamera =
+		Transform3d(
+			Translation3d(0.135, 0.375, -0.465),
+			Rotation3d(0.degrees.radians, 68.degrees.radians, 90.degrees.radians)
+		)
 	private val camera = PhotonCamera("AprilTag-Cam")
 
 	var aprilTags: AprilTagFieldLayout =
-		AprilTagFieldLayout(
-			listOf(
-				AprilTag(
-					5,
-					Pose3d(
-						0.0, 0.0, 0.0,
-						Rotation3d(0.0, 0.0, 0.0)
-					)
-				)
-			), 10.0, 10.0
-		)
+		AprilTagFields.k2024Crescendo.loadAprilTagLayoutField()
 
 	private val poseEstimator =
 		PhotonPoseEstimator(
@@ -44,26 +36,13 @@ object Vision : SubsystemBase() {
 	 * Gets the estimated robot position from the PhotonVision camera.
 	 * Returns null if it doesn't detect any April Tags.
 	 */
-	var estimatedGlobalPose: EstimatedRobotPose? = poseEstimator.update().orElse(null)
+	val estimatedGlobalPose: EstimatedRobotPose? get() = poseEstimator.update().orElse(null)
 
 	val latestResult get() = camera.latestResult
 
 	val bestTag get() = latestResult.bestTarget
 
-	fun calibrateTagPositions(transform: Transform3d) {
-		aprilTags = AprilTagFieldLayout(
-			latestResult.targets.map { target ->
-				AprilTag(
-					target.fiducialId,
-					(target.bestCameraToTarget + transform).let { Pose3d(it.translation, it.rotation) }
-				)
-			}, 10.0, 10.0
-		)
-	}
-
 	fun getTag(tagID: Int) = latestResult.getTargets().getOrNull(tagID)
 
-	override fun periodic() {
-		poseEstimator.update().orElse(null)
-	}
+
 }
