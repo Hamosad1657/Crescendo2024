baae00792bb018609a45b48d2abe4f4cac08dd3a
Hamosad-1657 <145373383+Hamosad-1657@users.noreply.github.com>
2024-02-13 22:46:42 +0200

    Formatted arm commands

--- src/main/kotlin/frc/robot/RobotContainer.kt
+++ src/main/kotlin/frc/robot/RobotContainer.kt
@@ -1,5 +1,6 @@
 package frc.robot
 
+import com.hamosad1657.lib.commands.finallyDo
 import com.hamosad1657.lib.math.simpleDeadband
 import com.pathplanner.lib.auto.AutoBuilder
 import com.pathplanner.lib.auto.NamedCommands
@@ -8,6 +9,11 @@ import edu.wpi.first.wpilibj2.command.*
 import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller
 import frc.robot.commands.openLoopTeleop_shooterAngle
 import frc.robot.commands.swerve.TeleopDriveCommand
+import frc.robot.subsystems.intake.IntakeConstants
+import frc.robot.subsystems.intake.IntakeSubsystem
+import frc.robot.subsystems.loader.LoaderConstants
+import frc.robot.subsystems.loader.LoaderSubsystem
+import frc.robot.subsystems.shooter.ShooterSubsystem
 import frc.robot.subsystems.swerve.SwerveSubsystem
 
 /**
@@ -51,27 +57,27 @@ object RobotContainer {
 		// Test 1 thing at a time.
 		// For a list of things to test follow the link:
 		// https://docs.google.com/document/d/1App5L-vltuqvOiloeHfqbKvk7FwQHXPcqmUYKuAhA1A/edit
-		
+
 		with(ShooterSubsystem) {
 			defaultCommand = openLoopTeleop_shooterAngle { testingController.leftY }
 //			defaultCommand = openLoopTeleop_shooterVelocity { testingController.leftY }
 		}
 
-//		with(IntakeSubsystem) {
-//			defaultCommand = run {
-//				set(IntakeConstants.MOTOR_OUTPUT)
-//			} finallyDo {
-//				set(0.0)
-//			}
-//		}
+		with(IntakeSubsystem) {
+			defaultCommand = run {
+				set(IntakeConstants.MOTOR_OUTPUT)
+			} finallyDo {
+				set(0.0)
+			}
+		}
 
-//		with(LoaderSubsystem) {
-//			defaultCommand = run {
-//				set(LoaderConstants.MOTOR_OUTPUT)
-//			} finallyDo {
-//				set(0.0)
-//			}
-//		}
+		with(LoaderSubsystem) {
+			defaultCommand = run {
+				set(LoaderConstants.MOTOR_OUTPUT)
+			} finallyDo {
+				set(0.0)
+			}
+		}
 	}
 
 	private fun registerAutoCommands() {
--- src/main/kotlin/frc/robot/commands/ArmCommands.kt
+++ src/main/kotlin/frc/robot/commands/ArmCommands.kt
@@ -1,47 +1,61 @@
 package frc.robot.commands
 
 import com.hamosad1657.lib.commands.*
-import com.hamosad1657.lib.units.FractionalOutput
+import com.hamosad1657.lib.units.PercentOutput
 import edu.wpi.first.wpilibj2.command.Command
 import frc.robot.subsystems.arm.ArmConstants.FOLD_OUTPUT
 import frc.robot.subsystems.arm.ArmConstants.OPEN_OUTPUT
 import frc.robot.subsystems.arm.ArmConstants.STAY_FOLDED_OUTPUT
 import frc.robot.subsystems.arm.ArmSubsystem
 
-/** - Requirements: arm. */
-fun ArmSubsystem.openCommand(): Command =
-	withName("open") {
-		run { setBothMotors(OPEN_OUTPUT) } until ::isAtLimit finallyDo { setBothMotors(0.0) }
+/** - Requirements: Arm. */
+fun ArmSubsystem.openCommand(): Command = withName("open") {
+	run {
+		setBothMotors(OPEN_OUTPUT)
+	} until ::isAtLimit finallyDo {
+		setBothMotors(0.0)
 	}
+}
 
-/** - Requirements: arm. */
-fun ArmSubsystem.foldCommand(): Command =
-	withName("fold") {
-		run { setBothMotors(FOLD_OUTPUT) } until ::isAtLimit finallyDo { setBothMotors(0.0) }
+/** - Requirements: Arm. */
+fun ArmSubsystem.foldCommand(): Command = withName("fold") {
+	run {
+		setBothMotors(FOLD_OUTPUT)
+	} until ::isAtLimit finallyDo {
+		setBothMotors(0.0)
 	}
+}
 
-
-fun ArmSubsystem.stayFoldedCommand(): Command =
-	withName("stay folded") {
-		run { setBothMotors(STAY_FOLDED_OUTPUT) } finallyDo { setBothMotors(0.0) }
+/** - Requirements: Arm. */
+fun ArmSubsystem.stayFoldedCommand(): Command = withName("stay folded") {
+	run {
+		setBothMotors(STAY_FOLDED_OUTPUT)
+	} finallyDo {
+		setBothMotors(0.0)
 	}
+}
 
-
+/** - Requirements: Arm. */
 fun ArmSubsystem.foldAndStayFoldedCommand(): Command =
-	withName("fold & stay folded") { foldCommand() andThen stayFoldedCommand() }
+	withName("fold & stay folded") {
+		foldCommand() andThen
+			stayFoldedCommand()
+	}
 
+/** - Requirements: Arm. */
 fun ArmSubsystem.separateSidesTeleopCommand(
-	leftOutput: () -> FractionalOutput,
-	rightOutput: () -> FractionalOutput
-): Command =
-	withName("separate sides teleop") {
-		run {
-			setLeftMotor(leftOutput())
-			setRightMotor(rightOutput())
-		} finallyDo { setBothMotors(0.0) }
+	leftOutput: () -> PercentOutput,
+	rightOutput: () -> PercentOutput
+): Command = withName("separate sides teleop") {
+	run {
+		setLeftMotor(leftOutput())
+		setRightMotor(rightOutput())
+	} finallyDo {
+		setBothMotors(0.0)
 	}
+}
 
-fun ArmSubsystem.bothSidesTeleopCommand(output: () -> FractionalOutput) =
-	withName("both sides teleop") {
-		separateSidesTeleopCommand(output, output)
-	}
+/** - Requirements: Arm. */
+fun ArmSubsystem.bothSidesTeleopCommand(output: () -> PercentOutput) = withName("both sides teleop") {
+	separateSidesTeleopCommand(output, output)
+}
