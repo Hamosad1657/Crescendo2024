af8f1d78292c1a5c8f32b4194af23cee733cbbf6
Hamosad <hamosadr@gmail.com>
2024-02-13 16:50:40 +0200

    Removed shooter physical limit switches

--- src/main/kotlin/frc/robot/subsystems/shooter/ShooterSubsystem.kt
+++ src/main/kotlin/frc/robot/subsystems/shooter/ShooterSubsystem.kt
@@ -4,14 +4,9 @@ import com.ctre.phoenix6.configs.CANcoderConfiguration
 import com.ctre.phoenix6.configs.FeedbackConfigs
 import com.ctre.phoenix6.controls.PositionVoltage
 import com.ctre.phoenix6.hardware.CANcoder
-import com.ctre.phoenix6.signals.AbsoluteSensorRangeValue
-import com.ctre.phoenix6.signals.FeedbackSensorSourceValue
-import com.ctre.phoenix6.signals.NeutralModeValue
-import com.ctre.phoenix6.signals.SensorDirectionValue
+import com.ctre.phoenix6.signals.*
 import com.hamosad1657.lib.motors.HaTalonFX
-import com.hamosad1657.lib.units.AngularVelocity
-import com.hamosad1657.lib.units.FractionalOutput
-import com.hamosad1657.lib.units.toIdleMode
+import com.hamosad1657.lib.units.*
 import com.revrobotics.CANSparkBase
 import com.revrobotics.CANSparkFlex
 import com.revrobotics.CANSparkLowLevel.MotorType
@@ -49,7 +44,6 @@ object ShooterSubsystem : SubsystemBase() {
 				RotorToSensorRatio = ANGLE_MOTOR_TO_CANCODER_GEAR_RATIO
 			}
 		)
-		configurator.apply(Constants.FALCON_HARDWARE_LIMITS_CONFIG)
 	}
 
 	private val angleCANCoder = CANcoder(ShooterMap.Angle.CANCODER_ID).apply {
@@ -123,10 +117,10 @@ object ShooterSubsystem : SubsystemBase() {
 	//  If they are wired normally closed, replace the 0 with a 1 and delete this comment.
 
 	val isAtMaxAngleLimit: Boolean
-		get() = angleMotor.getForwardLimit().value.value == 0
+		get() = angleCANCoder.absolutePosition.value >= Constants.MAX_POSSIBLE_ANGLE.rotations
 
 	val isAtMinAngleLimit: Boolean
-		get() = angleMotor.getReverseLimit().value.value == 0
+		get() = angleCANCoder.absolutePosition.value <= Constants.MIN_POSSIBLE_ANGLE.rotations
 
 	val isWithinVelocityTolerance get() = velocity - velocitySetpoint <= Constants.VELOCITY_TOLERANCE
 
