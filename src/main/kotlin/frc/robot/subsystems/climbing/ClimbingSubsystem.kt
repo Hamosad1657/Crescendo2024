f81678913f2d3dd22b1f350c043bc9ebedafe01c
Hamosad <hamosadr@gmail.com>
2024-02-16 10:42:52 +0200

    Shaked & Hagar: Fixed climbing limits.

--- src/main/kotlin/frc/robot/subsystems/climbing/ClimbingSubsystem.kt
+++ src/main/kotlin/frc/robot/subsystems/climbing/ClimbingSubsystem.kt
@@ -76,10 +76,10 @@ object ClimbingSubsystem : SubsystemBase() {
 
 	// TODO: Check if switches are wired normally open or normally closed.
 
-	private val isLeftAtClosedLimit get() = !leftClosedLimitSwitch.get()
-	private val isLeftAtOpenedLimit get() = !leftOpenedLimitSwitch.get()
+	private val isLeftAtClosedLimit get() = leftClosedLimitSwitch.get()
+	private val isLeftAtOpenedLimit get() = leftOpenedLimitSwitch.get()
 	private val isRightAtClosedLimit get() = rightClosedLimitSwitch.get()
-	private val isRightAtOpenedLimit get() = !rightOpenedLimitSwitch.get()
+	private val isRightAtOpenedLimit get() = rightOpenedLimitSwitch.get()
 
 	val isAtClosedLimit get() = isLeftAtClosedLimit || isRightAtClosedLimit
 	val isAtOpenedLimit get() = isLeftAtOpenedLimit || isRightAtOpenedLimit
