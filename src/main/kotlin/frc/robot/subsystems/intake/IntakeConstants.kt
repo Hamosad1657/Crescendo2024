c36b5d3de4e479e86081f0fa8713e2b10e2a911b
Hamosad <hamosadr@gmail.com>
2024-02-16 16:28:54 +0200

    Shaked: Lowered bottom intake motor voltage so the note does't get stuck around

--- src/main/kotlin/frc/robot/subsystems/intake/IntakeConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/IntakeConstants.kt
@@ -3,6 +3,6 @@ package frc.robot.subsystems.intake
 import com.hamosad1657.lib.units.Volts
 
 object IntakeConstants {
-	const val BOTTOM_MOTOR_VOLTAGE: Volts = 10.0
+	const val BOTTOM_MOTOR_VOLTAGE: Volts = 8.0
 	const val TOP_MOTOR_VOLTAGE: Volts = 12.0
 }
