9e877438eaaac8efe99b78a26d49f26aebc96e41
Sufilevy <sufilevy@gmail.com>
2024-03-02 22:08:22 +0200

    Reformatted DynamicShooting

--- src/main/kotlin/com/hamosad1657/lib/math/LinearInterpolationTable.kt
+++ src/main/kotlin/com/hamosad1657/lib/math/LinearInterpolationTable.kt
@@ -1,66 +1,36 @@
 package com.hamosad1657.lib.math
 
-import java.awt.geom.Point2D
+typealias Point = Pair<Double, Double>
 
-/* This should be updated to use binary search at some point. */
-class LinearInterpolationTable(vararg points: Point2D) {
-	private var m_maxInput = Double.NEGATIVE_INFINITY
-	private var m_minInput = Double.POSITIVE_INFINITY
-	val table: Array<Point2D>
-	val size: Int
+class LinearInterpolationTable(vararg points: Point) {
+	private val table: Array<out Point> = points
 
-	init {
-		table = points as Array<Point2D>
-		size = table.size
-		for (i in 0 until size) {
-			if (table[i].x > m_maxInput) {
-				m_maxInput = table[i].x
-			}
-			if (table[i].x < m_minInput) {
-				m_minInput = table[i].x
-			}
-		}
-	}
+	private var minInput = table.minOf { it.first }
+	private var maxInput = table.maxOf { it.first }
 
-	fun getOutput(input: Double): Double {
+	fun getOutputFor(input: Double): Double {
 		var index = 0
-		if (input <= m_minInput) {
-			index = 0
-		} else if (input >= m_maxInput) {
-			index = size - 2
-		} else {
+
+		if (input <= minInput) index = 0
+		else if (input >= maxInput) index = table.size - 2
+		else {
 			for (i in 1 until table.size) {
-				if (input > table[i - 1].x && input <= table[i].x) {
-					index = i - 1
-				}
+				if (input > table[i - 1].first && input <= table[i].first) index = i - 1
 			}
 		}
+
 		return interpolate(input, table[index], table[index + 1])
 	}
 
-	val x: DoubleArray
-		get() {
-			val xVals = DoubleArray(size)
-			for (i in 0 until size) {
-				xVals[i] = table[i].x
-			}
-			return xVals
-		}
-	val y: DoubleArray
-		get() {
-			val yVals = DoubleArray(size)
-			for (i in 0 until size) {
-				yVals[i] = table[i].y
-			}
-			return yVals
-		}
+	val firsts get() = table.map { it.first }.toDoubleArray()
+	val seconds get() = table.map { it.second }.toDoubleArray()
 
 	companion object {
-		fun interpolate(input: Double, point1: Point2D, point2: Point2D): Double {
-			val slope = (point2.y - point1.y) / (point2.x - point1.x)
-			val delta_x = input - point1.x
-			val delta_y = delta_x * slope
-			return point1.y + delta_y
+		private fun interpolate(input: Double, point1: Point, point2: Point): Double {
+			val slope = (point2.second - point1.second) / (point2.first - point1.first)
+			val deltaFirst = input - point1.first
+			val deltaSecond = deltaFirst * slope
+			return point1.second + deltaSecond
 		}
 	}
 }
--- src/main/kotlin/frc/robot/subsystems/shooter/DynamicShooting.kt
+++ src/main/kotlin/frc/robot/subsystems/shooter/DynamicShooting.kt
@@ -1,15 +1,16 @@
 package frc.robot.subsystems.shooter
 
-import com.hamosad1657.lib.math.*
-import com.hamosad1657.lib.robotAlliance
+import com.hamosad1657.lib.math.LinearInterpolationTable
+import com.hamosad1657.lib.math.clamp
+import com.hamosad1657.lib.math.mapRange
 import com.hamosad1657.lib.units.degrees
 import com.hamosad1657.lib.units.rpm
 import edu.wpi.first.math.geometry.Translation2d
 import edu.wpi.first.wpilibj.DriverStation.Alliance
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
+import frc.robot.Robot
 import frc.robot.subsystems.shooter.ShooterConstants.ShooterState
 import frc.robot.subsystems.vision.Vision
-import java.awt.geom.Point2D
 
 /**
  * - The position and distance units are meters.
@@ -17,6 +18,8 @@ import java.awt.geom.Point2D
  * - The velocity units are RPM.
  */
 object DynamicShooting {
+	// --- Constants ---
+
 	private const val MIN_DISTANCE_TO_SPEAKER = 2.5 // Meters
 	private const val MAX_DISTANCE_TO_SPEAKER = 5.8 // Meters
 
@@ -26,37 +29,23 @@ object DynamicShooting {
 	private const val MIN_VELOCITY = 3000.0 // RPM
 	private const val MAX_VELOCITY = 4000.0 // RPM
 
-	private val ANGLE_INTERPOLATION_TABLE = LinearInterpolationTable(
-		Point2D.Double(0.0, MAX_ANGLE),
-		Point2D.Double(0.414600, 167.0),
-		Point2D.Double(1.0, MIN_ANGLE)
-	)
+	private val ANGLE_INTERPOLATION_TABLE =
+		LinearInterpolationTable(
+			0.0 to MAX_ANGLE,
+			0.415 to 167.0,
+			1.0 to MIN_ANGLE,
+		)
 
-	val speakerPosition
-		get() = when (robotAlliance) {
-			Alliance.Red -> SPEAKER_RED_POSITION_METERS
-			Alliance.Blue -> SPEAKER_BLUE_POSITION_METERS
-		}
-
-	val speakerTagId
-		get() = when (robotAlliance) {
-			Alliance.Red -> SPEAKER_RED_TAG_ID
-			Alliance.Blue -> SPEAKER_BLUE_TAG_ID
-		}
-
-	val seesSpeakerTag
-		get() = Vision.getTag(speakerTagId) != null
-
+	// --- Calculations ---
 
 	/** This function assumes the robot is directly facing the speaker. */
 	fun calculateShooterState(robotPosition: Translation2d): ShooterState {
 		val robotToSpeakerFlatDistance = robotPosition.getDistance(speakerPosition)
-		SmartDashboard.putNumber(
-			"robot to speaker distance", robotToSpeakerFlatDistance
-		)
-		val distanceToSpeaker01 = distanceToSpeaker01(robotToSpeakerFlatDistance)
+			.also { SmartDashboard.putNumber("Robot to speaker distance", it) }
 
-		SmartDashboard.putNumber("Dynamic shooting factor", distanceToSpeaker01)
+		val distanceToSpeaker01 = distanceToSpeaker01(robotToSpeakerFlatDistance)
+			.also { SmartDashboard.putNumber("Dynamic shooting factor", it) }
+
 		return ShooterState(
 			calculateAngleSetpoint(distanceToSpeaker01).degrees,
 			calculateVelocitySetpoint(distanceToSpeaker01).rpm,
@@ -66,49 +55,56 @@ object DynamicShooting {
 	/** Calculate the distance from the shooter as a 0.0->1.0 factor. */
 	private fun distanceToSpeaker01(shooterToSpeakerFlatDistance: Double): Double {
 		val distance = mapRange(
-			shooterToSpeakerFlatDistance,
-			MIN_DISTANCE_TO_SPEAKER,
-			MAX_DISTANCE_TO_SPEAKER,
-			0.0,
-			1.0
+			value = shooterToSpeakerFlatDistance,
+			startMin = MIN_DISTANCE_TO_SPEAKER,
+			startMax = MAX_DISTANCE_TO_SPEAKER,
+			endMin = 0.0,
+			endMax = 1.0,
 		)
 		return clamp(distance, min = 0.0, max = 1.0)
 	}
 
 	/** Calculate the required shooter angle from the 0.0->1.0 distance factor. */
 	private fun calculateAngleSetpoint(distanceToSpeaker01: Double): Double {
-		//val factor = 1 - sqrt(distanceToSpeaker01)
-//		val factor = 1 - distanceToSpeaker01
-		val angle = ANGLE_INTERPOLATION_TABLE.getOutput(distanceToSpeaker01)
-		SmartDashboard.putNumber(
-			"Dynamic shooting angle",
-			clamp(angle, MIN_ANGLE, MAX_ANGLE)
-		)
+		val angle = ANGLE_INTERPOLATION_TABLE.getOutputFor(distanceToSpeaker01)
 		return clamp(angle, MIN_ANGLE, MAX_ANGLE)
+			.also { SmartDashboard.putNumber("Dynamic shooting angle", it) }
 	}
 
 	/** Calculate the required shooter setpoint from the 0.0->1.0 distance factor. */
 	private fun calculateVelocitySetpoint(distanceToSpeaker01: Double): Double {
 		val rpm = distanceToSpeaker01 * (MAX_VELOCITY - MIN_VELOCITY) + MIN_VELOCITY
 		return clamp(rpm, MIN_VELOCITY, MAX_VELOCITY)
+			.also { SmartDashboard.putNumber("Dynamic shooting velocity", it) }
 	}
 
-	/**
-	 * The Translation2d of the blue speaker if you are in the blue alliance, according to WPILib's
-	 * field coordinate system.
-	 */
+
+	// --- Speaker Info ---
+
+	val seesSpeakerTag get() = Vision.getTag(speakerTagId) != null
+
+	/** The Translation2d of the blue speaker if you are in the blue alliance, according to WPILib's field coordinate system. */
 	private val SPEAKER_BLUE_POSITION_METERS = Translation2d(-0.04, 5.55) // Meters
 
-	/**
-	 * The Translation2d of the red speaker if you are in the red alliance, according to WPILib's
-	 * field coordinate system.
-	 */
+	/** The Translation2d of the red speaker if you are in the red alliance, according to WPILib's field coordinate system. */
 	private val SPEAKER_RED_POSITION_METERS = Translation2d(16.58, 5.55) // Meters
 
-	/**id of the april tag in the center of the speaker*/
-	const val SPEAKER_RED_TAG_ID = 4
+	/** ID of the april tag in the center of the speaker. */
+	private const val SPEAKER_RED_TAG_ID = 4
 
-	/**id of the april tag in the center of the speaker*/
-	const val SPEAKER_BLUE_TAG_ID = 7
+	/** ID of the april tag in the center of the speaker. */
+	private const val SPEAKER_BLUE_TAG_ID = 7
+
+	val speakerPosition
+		get() = when (Robot.alliance) {
+			Alliance.Red -> SPEAKER_RED_POSITION_METERS
+			Alliance.Blue -> SPEAKER_BLUE_POSITION_METERS
+		}
+
+	val speakerTagId
+		get() = when (Robot.alliance) {
+			Alliance.Red -> SPEAKER_RED_TAG_ID
+			Alliance.Blue -> SPEAKER_BLUE_TAG_ID
+		}
 }
 
