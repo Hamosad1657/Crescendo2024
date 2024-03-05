d907ddce6690b61ead358aee2cf5b9903b48e7fa
Hamosad <hamosadr@gmail.com>
2024-03-05 12:48:58 +0200

    Shaked: Changed at stage rotation to Itay's request

--- src/main/kotlin/frc/robot/subsystems/swerve/SwerveConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/swerve/SwerveConstants.kt
@@ -43,9 +43,9 @@ object SwerveConstants {
 
 	val AT_STAGE_TO_SPEAKER_ROTATION
 		get() =
-			if (Robot.alliance == DriverStation.Alliance.Blue) 5.0.degrees
-			else -5.0.degrees
-
+			if (Robot.alliance == DriverStation.Alliance.Blue) 10.0.degrees
+			else -10.0.degrees
+	
 
 	// --- PID ---
 
