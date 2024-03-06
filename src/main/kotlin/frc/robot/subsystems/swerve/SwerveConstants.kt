125076b18c46fa1b71d29f96f18934fd26f2bb51
Hamosad-1657 <145373383+Hamosad-1657@users.noreply.github.com>
2024-03-06 18:33:52 +0200

    Aviv (Shaked writing this): Tuned path PID

--- src/main/kotlin/frc/robot/subsystems/swerve/SwerveConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/swerve/SwerveConstants.kt
@@ -94,8 +94,8 @@ object SwerveConstants {
 
 	// --- PathPlanner ---
 
-	private val PATH_TRANSLATION_CONSTANTS = PIDConstants(20.0, 0.0, 0.0)
-	private val PATH_ROTATION_CONSTANTS = PIDConstants(23.0, 0.0, 0.00)
+	private val PATH_TRANSLATION_CONSTANTS = PIDConstants(19.0, 0.0, 0.0)
+	private val PATH_ROTATION_CONSTANTS = PIDConstants(21.5, 0.0, 0.00)
 
 	// TODO: Tune.
 	val PATH_CONSTRAINTS = PathConstraints(
