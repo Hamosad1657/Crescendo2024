aa19730fa9db2a299724c740414cd924318cb85e
Hamosad <hamosadr@gmail.com>
2024-03-14 14:19:54 +0200

    Shaked: Added LEDsMode

--- src/main/kotlin/frc/robot/subsystems/leds/LedsSubsystem.kt
+++ src/main/kotlin/frc/robot/subsystems/leds/LedsSubsystem.kt
@@ -4,6 +4,9 @@ import edu.wpi.first.wpilibj.AddressableLED
 import edu.wpi.first.wpilibj.AddressableLEDBuffer
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import frc.robot.RobotMap
+import frc.robot.subsystems.intake.IntakeSubsystem
+import frc.robot.subsystems.loader.LoaderSubsystem
+import frc.robot.subsystems.shooter.ShooterSubsystem
 import frc.robot.subsystems.leds.LedsConstants as Constants
 
 object LedsSubsystem : SubsystemBase() {
@@ -21,8 +24,12 @@ object LedsSubsystem : SubsystemBase() {
 		}
 	}
 
-	val ledsBuffer = AddressableLEDBuffer(Constants.LENGTH)
-	val ledStrip = AddressableLED(RobotMap.Leds.PWM_PORT).apply {
+	enum class LEDsMode {
+		COLLECT, SHOOT, BLINK_READY, DEFAULT,
+	}
+
+	private val ledsBuffer = AddressableLEDBuffer(Constants.LENGTH)
+	private val ledStrip = AddressableLED(RobotMap.Leds.PWM_PORT).apply {
 		setData(ledsBuffer)
 		start()
 	}
@@ -32,5 +39,29 @@ object LedsSubsystem : SubsystemBase() {
 			ledsBuffer.setRGB(i, color.red, color.green, color.blue)
 		}
 		ledStrip.setData(ledsBuffer)
+	}
+
+	private fun collectMode() {
+		if (IntakeSubsystem.isBottomMotorUnderLoad) {
+			setColor(RGBColor.MAGENTA)
+		} else {
+			setColor(RGBColor.WHITE)
+		}
+	}
+
+	private fun shooterMode() {
+		if (ShooterSubsystem.isWithinTolerance) {
+			setColor(RGBColor.MAGENTA)
+		} else {
+			setColor(RGBColor.WHITE)
+		}
+	}
+
+	private fun defaultMode() {
+		if (LoaderSubsystem.isNoteDetected) {
+			setColor(RGBColor.TEAM_GREEN)
+		} else {
+			setColor(RGBColor.DARK)
+		}
 	}
 }
