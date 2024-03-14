3f4c9a338ae85aa496cda0b56e9f0cf883a6cd4e
Hamosad <hamosadr@gmail.com>
2024-03-14 14:34:59 +0200

    Shaked: Refactored leds

--- src/main/kotlin/frc/robot/subsystems/leds/LedsConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/leds/LedsConstants.kt
@@ -1,5 +1,24 @@
 package frc.robot.subsystems.leds
 
 object LedsConstants {
+	data class RGBColor(val red: Int, val green: Int, val blue: Int) {
+		companion object {
+			val TEAM_GREEN = RGBColor(22, 87, 0)
+			val PURE_GREEN = RGBColor(0, 255, 0)
+			val WHITE = RGBColor(255, 255, 255)
+			val RED = RGBColor(255, 0, 0)
+			val BLUE = RGBColor(0, 0, 255)
+			val YELLOW = RGBColor(255, 255, 0)
+			val CYAN = RGBColor(0, 255, 255)
+			val MAGENTA = RGBColor(255, 0, 255)
+			val DARK = RGBColor(0, 0, 0)
+		}
+	}
+
+	enum class LEDsMode {
+		COLLECT, SHOOT, BLINK_READY, DEFAULT,
+	}
+
 	const val LENGTH = 0
+	const val READY_BLINK_TIME = 0.2
 }
--- src/main/kotlin/frc/robot/subsystems/leds/LedsSubsystem.kt
+++ src/main/kotlin/frc/robot/subsystems/leds/LedsSubsystem.kt
@@ -1,32 +1,20 @@
 package frc.robot.subsystems.leds
 
+import com.hamosad1657.lib.units.Seconds
 import edu.wpi.first.wpilibj.AddressableLED
 import edu.wpi.first.wpilibj.AddressableLEDBuffer
+import edu.wpi.first.wpilibj.Timer
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import frc.robot.RobotMap
 import frc.robot.subsystems.intake.IntakeSubsystem
+import frc.robot.subsystems.leds.LedsConstants.RGBColor
 import frc.robot.subsystems.loader.LoaderSubsystem
 import frc.robot.subsystems.shooter.ShooterSubsystem
 import frc.robot.subsystems.leds.LedsConstants as Constants
 
 object LedsSubsystem : SubsystemBase() {
-	data class RGBColor(val red: Int, val green: Int, val blue: Int) {
-		companion object {
-			val TEAM_GREEN = RGBColor(22, 87, 0)
-			val PURE_GREEN = RGBColor(0, 255, 0)
-			val WHITE = RGBColor(255, 255, 255)
-			val RED = RGBColor(255, 0, 0)
-			val BLUE = RGBColor(0, 0, 255)
-			val YELLOW = RGBColor(255, 255, 0)
-			val CYAN = RGBColor(0, 255, 255)
-			val MAGENTA = RGBColor(255, 0, 255)
-			val DARK = RGBColor(0, 0, 0)
-		}
-	}
 
-	enum class LEDsMode {
-		COLLECT, SHOOT, BLINK_READY, DEFAULT,
-	}
+	private val blinkTimer = Timer()
 
 	private val ledsBuffer = AddressableLEDBuffer(Constants.LENGTH)
 	private val ledStrip = AddressableLED(RobotMap.Leds.PWM_PORT).apply {
@@ -34,11 +22,34 @@ object LedsSubsystem : SubsystemBase() {
 		start()
 	}
 
-	fun setColor(color: RGBColor) {
+	val areLedsOn: Boolean
+		get() =
+			(ledsBuffer.getRed(0) != 0) or
+				(ledsBuffer.getGreen(0) != 0) or
+				(ledsBuffer.getBlue(0) != 0)
+
+	private fun setColor(color: RGBColor) {
 		for (i in 0..Constants.LENGTH) {
 			ledsBuffer.setRGB(i, color.red, color.green, color.blue)
 		}
 		ledStrip.setData(ledsBuffer)
+	}
+
+	private fun toggleLeds(colorIfOff: RGBColor) {
+		if (areLedsOn) {
+			setColor(RGBColor.DARK)
+		} else {
+			setColor(colorIfOff)
+		}
+	}
+
+	/** Should be called periodically. */
+	private fun blink(color: RGBColor, blinkTime: Seconds) {
+		blinkTimer.start()
+		if (blinkTimer.hasElapsed(blinkTime)) {
+			toggleLeds(color)
+			blinkTimer.restart()
+		}
 	}
 
 	private fun collectMode() {
@@ -63,5 +74,9 @@ object LedsSubsystem : SubsystemBase() {
 		} else {
 			setColor(RGBColor.DARK)
 		}
+	}
+
+	private fun blinkReadyMode() {
+		blink(RGBColor.PURE_GREEN, Constants.READY_BLINK_TIME)
 	}
 }
