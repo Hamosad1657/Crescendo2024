47f0938040e5ba870168b893e32024c539c862f0
Hamosad <hamosadr@gmail.com>
2024-03-14 10:05:09 +0200

    Shaked: Created blink command

--- src/main/kotlin/frc/robot/subsystems/leds/LedsSubsystem.kt
+++ src/main/kotlin/frc/robot/subsystems/leds/LedsSubsystem.kt
@@ -1,9 +1,12 @@
 package frc.robot.subsystems.leds
 
 import com.hamosad1657.lib.commands.*
+import com.hamosad1657.lib.units.Seconds
 import edu.wpi.first.wpilibj.AddressableLED
 import edu.wpi.first.wpilibj.AddressableLEDBuffer
+import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.SubsystemBase
+import edu.wpi.first.wpilibj2.command.WaitCommand
 import frc.robot.RobotMap
 import frc.robot.subsystems.leds.LedsConstants as Constants
 
@@ -33,7 +36,13 @@ object LedsSubsystem : SubsystemBase() {
 		ledStrip.setData(ledsBuffer)
 	}
 
-	fun setColorCommand(color: RGBColor) = withName("set color") {
+	fun setColorCommand(color: RGBColor): Command = withName("set color") {
 		runOnce { setColor(color) }
+	}
+
+	fun blinkCommand(color: RGBColor, blinkTime: Seconds): Command = withName("blink") {
+		(setColorCommand(color) alongWith WaitCommand(blinkTime)) andThen
+			(setColorCommand(RGBColor.DARK) alongWith WaitCommand(blinkTime))
+				.repeatedly()
 	}
 }
