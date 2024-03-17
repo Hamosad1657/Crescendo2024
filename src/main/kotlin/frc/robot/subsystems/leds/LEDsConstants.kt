f3be7762f130223b292848670c7f43886d9c602e
Hamosad <hamosadr@gmail.com>
2024-03-17 18:41:13 +0200

    Shaked: Modified led blink time

--- src/main/kotlin/frc/robot/subsystems/leds/LEDsConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/leds/LEDsConstants.kt
@@ -2,10 +2,11 @@ package frc.robot.subsystems.leds
 
 object LEDsConstants {
 	const val LENGTH = 24
-
-	const val ACTION_FINISHED_MODE_BLINK_TIME = 0.15
-	const val DYNAMIC_SHOOTING_FAILED_BLINK_TIME = 0.1
+	
 	const val ACTION_FINISHED_MODE_TIMEOUT = 1.0
+
+	const val ACTION_FINISHED_MODE_BLINK_TIME = 0.1
+	const val DYNAMIC_SHOOTING_FAILED_BLINK_TIME = 0.1
 
 	const val WAIT_WITH_NOTE_DELAY = 0.15
 
